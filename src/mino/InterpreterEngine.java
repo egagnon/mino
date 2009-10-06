105f68ac79939ce81e93a575df9ca200b4d426d1
Etienne M. Gagnon <egagnon@j-meg.com>
2009-10-06 17:23:37 -0400

    Add initial interpreter class.
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

---
+++ src/mino/InterpreterEngine.java
@@ -0,0 +1,45 @@
+/* This file is part of Mino.
+ *
+ * See the NOTICE file distributed with this work for copyright information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mino;
+
+import mino.syntax.analysis.DepthFirstAdapter;
+import mino.syntax.node.AClassdef;
+import mino.syntax.node.AFieldMember;
+import mino.syntax.node.Switch;
+
+public class InterpreterEngine
+        extends DepthFirstAdapter
+        implements Switch {
+
+    private MinoClass currentClass;
+
+    @Override
+    public void inAClassdef(
+            AClassdef node) {
+
+        this.currentClass = MinoClass.addClass(node);
+    }
+
+    @Override
+    public void outAFieldMember(
+            AFieldMember node) {
+
+        this.currentClass.addField(node);
+    }
+
+}
---
+++ src/mino/MinoInterpreter.java
@@ -0,0 +1,41 @@
+/* This file is part of Mino.
+ *
+ * See the NOTICE file distributed with this work for copyright information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mino;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import mino.syntax.lexer.Lexer;
+import mino.syntax.lexer.LexerException;
+import mino.syntax.node.Node;
+import mino.syntax.parser.Parser;
+import mino.syntax.parser.ParserException;
+
+public class MinoInterpreter {
+
+    public static void main(
+            String[] args)
+            throws ParserException, LexerException, IOException {
+
+        Parser parser = new Parser(new Lexer(new FileReader(args[0])));
+        Node syntaxTree = parser.parse();
+
+        syntaxTree.apply(new InterpreterEngine());
+    }
+
+}
