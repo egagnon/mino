1cbebf8a5ccdd829cb4d3bc674606359f25f36f3
Etienne M. Gagnon <egagnon@j-meg.com>
2009-10-29 23:58:09 -0400

    Indent code.
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

--- src/mino/structure/PrimitiveNormalMethodInfo.java
+++ src/mino/structure/PrimitiveNormalMethodInfo.java
@@ -47,8 +47,8 @@ public class PrimitiveNormalMethodInfo
         String className = methodTable.getClassInfo().getName();
         if (className.equals("Object") && getName().equals("abort")) {
             if (params.size() != 1) {
-                throw new InterpreterException("abort method has one parameter",
-                        definition.getId());
+                throw new InterpreterException(
+                        "abort method has one parameter", definition.getId());
             }
             this.operation = Operation.OBJECT_ABORT;
         }
